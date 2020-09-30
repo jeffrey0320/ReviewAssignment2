7082a855ad1d1e5857a018e7400f744ac11b6136
jeffrey lopez <jeffreysofly0320@gmail.com>
2020-09-29 20:00:37 -0400

    Initial commit

---
+++ src/Review2.java
@@ -0,0 +1,5 @@
+public class Review2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/personnelData.java
@@ -0,0 +1,6 @@
+public class personnelData {
+    private String employeeNumber;
+    private String lastName;
+    private String firstName;
+    private String streetAddress;
+}
