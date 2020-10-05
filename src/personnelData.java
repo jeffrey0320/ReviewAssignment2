9322488140156117203c5a9d47945f07ecdd3fb3
jeffrey lopez <jeffreysofly0320@gmail.com>
2020-10-05 16:24:29 -0400

    new

--- src/personnelData.java
+++ src/personnelData.java
@@ -3,4 +3,41 @@ public class personnelData {
     private String lastName;
     private String firstName;
     private String streetAddress;
+    private String cityStateZip;
+    private double rateBefore9PM;
+    private double rateBet9and12;
+    private double rateAfter12;
+
+
+    public String getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public String getCityStateZip() {
+        return cityStateZip;
+    }
+
+    public double getRateBefore9PM() {
+        return rateBefore9PM;
+    }
+
+    public double getRateBet9and12() {
+        return rateBet9and12;
+    }
+
+    public double getRateAfter12() {
+        return rateAfter12;
+    }
 }
